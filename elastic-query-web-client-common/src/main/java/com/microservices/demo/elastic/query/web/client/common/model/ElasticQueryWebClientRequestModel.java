41fa83eda9b2b0d2c902708299f181c0b93eb629
Maksym_Kruhovykh <18maxim1998@gmail.com>
2023-08-06 12:47:54 +0300

    Lesson64: move common classes to common modules

--- elastic-query-web-client-common/src/main/java/com/microservices/demo/elastic/query/web/client/common/model/ElasticQueryWebClientRequestModel.java
+++ elastic-query-web-client-common/src/main/java/com/microservices/demo/elastic/query/web/client/common/model/ElasticQueryWebClientRequestModel.java
@@ -1,15 +1,18 @@
-package com.microservices.demo.elastic.query.service.model;
+package com.microservices.demo.elastic.query.web.client.common.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ElasticQueryServiceRequestModel {
+public class ElasticQueryWebClientRequestModel {
     private String id;
+    @NotEmpty
     private String text;
 }
